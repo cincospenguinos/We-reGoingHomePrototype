35a14e86a5d206e9954659091667128d68f4ac88
Andre LaFleur <andre.lafleur@utah.edu>
2019-01-25 16:38:56 -0700

    Add another spec to puzzleSolver

--- assets/js/src/helpers/puzzleSolver.js
+++ assets/js/src/helpers/puzzleSolver.js
@@ -4,6 +4,7 @@
  * Class that handles solving the puzzle, and indicates changes of state.
  */
 import { Target } from '../model/target.js';
+import { Direction } from '../model/direction.js';
 
 export class PuzzleSolver {
 	constructor(puzzle) {
@@ -25,7 +26,8 @@ export class PuzzleSolver {
 			this.puzzleState.current.laserPaths[laser.key] = this._determineLaserPath(laser);
 		});
 
-		// 2. Calculate the trimmed path of each laser
+		this._trimLaserPaths();
+
 		// 3. Establish which targets are hit by what lasers
 		// 4. Manage modification of exits according to targets hit by lasers
 		// 5. Check the validity of the puzzle to ensure that it's acceptable
@@ -83,6 +85,25 @@ export class PuzzleSolver {
 		return path;
 	}
 
+	/** Helper method. Trim the laser's path through the puzzle, ensuring that only like colored lasers may cross. */
+	_trimLaserPaths() {
+		Object.keys(this.puzzleState.current.laserPaths).forEach((laser1key) => {
+			Object.keys(this.puzzleState.current.laserPaths)
+				.filter((k) => k !== laser1key)
+				.forEach((laser2key) => {
+					let laser1Path = this.puzzleState.current.laserPaths[laser1key];
+					let laser2Path = this.puzzleState.current.laserPaths[laser2key];
+
+					const intersection = this._getIntersectionBetween(laser1Path, laser2Path);
+
+					if (intersection) {
+						this._removeStrikingLaserFromState(laser1key);
+						this._removeStrikingLaserFromState(laser2key);
+					}
+				});
+		});
+	}
+
 	/** Helper method. Returns the closest item in the list that the laser is intersecting, or null if none exists. */
 	_findClosestItem(origin, direction) {
 		let relevantItems = this.puzzle.getLaserInteractable().filter((s) => s.getLaserCollisionPoint(origin, direction) !== null);
@@ -106,16 +127,16 @@ export class PuzzleSolver {
 		let newPoint = { x: point.x, y: point.y };
 		switch(direction) {
 		case Direction.EAST:
-			newPoint.x = this.dimensions.width + this.translation.x;
+			newPoint.x = this.puzzle.dimensions.width + this.puzzle.translation.x;
 			break;
 		case Direction.SOUTH:
-			newPoint.y = this.dimensions.height + this.translation.y;
+			newPoint.y = this.puzzle.dimensions.height + this.puzzle.translation.y;
 			break;
 		case Direction.WEST:
-			newPoint.x = this.translation.x;
+			newPoint.x = this.puzzle.translation.x;
 			break;
 		case Direction.NORTH:
-			newPoint.y = this.translation.y;
+			newPoint.y = this.puzzle.translation.y;
 			break;
 		}
 		return newPoint;
@@ -130,6 +151,94 @@ export class PuzzleSolver {
 		}
 
 		strikingLasers[targetKey].push(laserKey);
+	}
+
+	/** Helper method. Removes laser matching key provided from any target in the state. */
+	_removeStrikingLaserFromState(laserKey) {
+		const strikingLasers = this.puzzleState.current.strikingLasers;
+		Object.keys(strikingLasers).forEach((targetKey) => {
+			strikingLasers[targetKey] = strikingLasers[targetKey].filter(l => l === laserKey);
+		});
+	}
+
+	/** Helper method. Returns intersection between the two lasers provided, or null if none exists.*/
+	_getIntersectionBetween(path1, path2) {
+		let intersection = null;
+
+		this._pathAsLines(path1).forEach((line1) => {
+			this._pathAsLines(path2).forEach((line2) => {
+				intersection = this._intersectionPointOf(line1, line2);
+
+				if (intersection) {
+					return;
+				}
+			});
+
+			if (intersection) return;
+		});
+
+		return intersection;
+	}
+
+	/** Helper method. Returns path provided as a collection of lines. */
+	_pathAsLines(path) {
+		let newPath = [];
+
+		for (let i = 0; i < path.length - 1; i++) {
+			newPath.push({ 
+				x1: path[i].x, 
+				y1: path[i].y, 
+				x2: path[i + 1].x, 
+				y2: path[i + 1].y,
+				isHorizontal: path[i].y === path[i + 1].y
+			});
+		}
+
+		return newPath;
+	}
+
+	/** Helper method. Returns intersection point of the two lines provided, or null if none exists. */
+	_intersectionPointOf(line1, line2) {
+		let line1Horizontal = (line1.y1 === line1.y2);
+		let line2Horizontal = (line2.y1 === line2.y2);
+
+		// First case: both lines face the same direction
+		if ((line1Horizontal && line2Horizontal) || (!line1Horizontal && !line2Horizontal)) {
+			if (line1Horizontal) {
+				if ((line1.x1 <= Math.max(line2.x1, line2.x2) && line1.x1 >= Math.min(line2.x1, line2.x2)) ||
+					(line1.x2 <= Math.max(line2.x1, line2.x2) && line1.x2 >= Math.min(line2.x1, line2.x2))) {
+					// This is just the mid-point of all of their mins/maxes
+					let x1 = Math.min(line1.x1, line1.x2, line2.x1, line2.x2);
+					let x2 = Math.max(line1.x1, line1.x2, line2.x1, line2.x2);
+
+					return { x: (x2 + x1) / 2, y: line1.y1 }
+				}
+			} else {
+				if ((line1.y1 <= Math.max(line2.y1, line2.y2) && line1.y1 >= Math.min(line2.y1, line2.y2)) ||
+					(line1.y2 <= Math.max(line2.y1, line2.y2) && line1.y2 >= Math.min(line2.y1, line2.y2))) {
+					let y1 = Math.min(line1.y1, line1.y2, line2.y1, line2.y2);
+					let y2 = Math.min(line1.y1, line1.y2, line2.y1, line2.y2);
+
+					return { x: line1.x1, y: (y1 + y2) / 2 };
+				}
+			}
+		} else { // Second case: both lines are opposite directions
+			if (line1Horizontal) {
+				let betweenLine = (line2.x1 <= Math.max(line1.x1, line1.x2) && line2.x1 >= Math.min(line1.x1, line1.x2));
+
+				if (Math.min(line2.y1, line2.y2) <= line1.y1 && Math.max(line2.y1, line2.y2) >= line1.y1) {
+					return { x: line2.x1, y: line1.y1 };
+				}
+			} else {
+				let betweenLine = (line1.x1 <= Math.max(line2.x1, line2.x2) && line1.x1 >= Math.min(line2.x1, line2.x2));
+
+				if (Math.min(line1.y1, line1.y2) <= line2.y1 && Math.max(line1.y1, line1.y2) >= line2.y1) {
+					return { x: line1.x1, y: line2.y1 };
+				}
+			}
+		}
+
+		return null;
 	}
 
 	/** Helper method. Sets the internals of puzzle object to match the current state object. */
--- assets/js/test/helpers/puzzleSolverTest.js
+++ assets/js/test/helpers/puzzleSolverTest.js
@@ -5,41 +5,91 @@ import { TestHelper } from '../testHelper.js';
 import { PuzzleSolver } from '../../src/helpers/puzzleSolver.js';
 
 QUnit.module('PuzzleSolver', () => {
-	QUnit.test('indicates that a puzzle is solved', (assert) => {
-		let puzzle = TestHelper.createPuzzle();
-		puzzle.setTranslation({ x: 0, y: 0 });
 
-		let laser = TestHelper.createLaser({
-			direction: TestHelper.directions.east,
-			position: { x: 10, y: 10 },
-			dimensions: { width: 0, height: 0 },
-		});
-		let mirror = TestHelper.createSurface({
-			position: { x: 100, y: 15 },
-			dimensions: { width: 20, height: 20 },
-			direction: TestHelper.directions.west,
-		});
-		let target = TestHelper.createTarget({
-			position: { x: 90, y: 100 },
-			dimensions: { width: 20, height: 20 },
-		});
-		let exit = TestHelper.createExit({
-			direction: TestHelper.directions.east,
-			position: { x: 100, height: 190},
-			dimensions: { width: 10, height: 10 },
-		});
+	QUnit.module('single laser puzzle', (assert) => {
+		QUnit.test('solves a puzzle with a single laser', (assert) => {
+			let puzzle = TestHelper.createPuzzle();
+			puzzle.setTranslation({ x: 0, y: 0 });
 
-		puzzle.addLaser(laser);
-		puzzle.addSurface(mirror);
-		puzzle.addExit(exit);
-		puzzle.addTarget(target);
+			let laser = TestHelper.createLaser({
+				direction: TestHelper.directions.east,
+				position: { x: 10, y: 10 },
+				dimensions: { width: 0, height: 0 },
+			});
+			let mirror = TestHelper.createSurface({
+				position: { x: 100, y: 15 },
+				dimensions: { width: 20, height: 20 },
+				direction: TestHelper.directions.west,
+			});
+			let target = TestHelper.createTarget({
+				position: { x: 90, y: 100 },
+				dimensions: { width: 20, height: 20 },
+			});
+			let exit = TestHelper.createExit({
+				direction: TestHelper.directions.east,
+				position: { x: 100, height: 190},
+				dimensions: { width: 10, height: 10 },
+			});
 
-		const puzzleSolver = new PuzzleSolver(puzzle);
-		puzzleSolver.solve();
+			puzzle.addLaser(laser);
+			puzzle.addSurface(mirror);
+			puzzle.addExit(exit);
+			puzzle.addTarget(target);
 
-		assert.ok(puzzle.lasers['laser'].path, 'Laser should have a path assigned to it');
-		assert.ok(puzzle.targets['target'].isLit(), 'Target should be lit');
-		assert.ok(puzzle.targets['target'].isStruckBy(TestHelper.laserColors.red), 'Target should be struck by red laser');
-		assert.ok(puzzle.exits['exit'].isOpen, 'Exit should be open');
+			const puzzleSolver = new PuzzleSolver(puzzle);
+			puzzleSolver.solve();
+
+			assert.ok(puzzle.lasers['laser'].path, 'Laser should have a path assigned to it');
+			assert.ok(puzzle.targets['target'].isLit(), 'Target should be lit');
+			assert.ok(puzzle.targets['target'].isStruckBy(TestHelper.laserColors.red), 'Target should be struck by red laser');
+			assert.ok(puzzle.exits['exit'].isOpen, 'Exit should be open');
+		});
 	});
+
+	QUnit.module('multi-laser puzzle', (assert) => {
+		QUnit.test('puzzle unsolved when lasers crossed', (assert) => {
+			let puzzle = TestHelper.createPuzzle();
+			puzzle.setTranslation({ x: 0, y: 0 });
+
+			let laser1 = TestHelper.createLaser({
+				direction: TestHelper.directions.east,
+				position: { x: 50, y: 50 },
+				dimensions: { width: 0, height: 0 },
+			});
+
+			let laser2 = TestHelper.createLaser({
+				key: 'otherlaser',
+				position: { x: 80, y: 10 },
+				dimensions: { width: 0, height: 0 },
+				color: TestHelper.laserColors.green,
+				direction: TestHelper.directions.south,
+			})
+
+			let target = TestHelper.createTarget({
+				position: { x: 100, y: 55 },
+				dimensions: { width: 20, height: 20 },
+			});
+
+			let exit = TestHelper.createExit({
+				direction: TestHelper.directions.east,
+				position: { x: 100, height: 190},
+				dimensions: { width: 10, height: 10 },
+			});
+
+			puzzle.addLaser(laser1);
+			puzzle.addLaser(laser2);
+			puzzle.addTarget(target);
+			puzzle.addExit(exit);
+
+			const puzzleSolver = new PuzzleSolver(puzzle);
+			puzzleSolver.solve();
+
+			assert.ok(puzzle.lasers['laser'].path, 'Laser should have a path assigned to it');
+			assert.ok(puzzle.lasers['otherlaser'].path, 'Other laser should have a path assigned to it');
+			assert.notOk(puzzle.targets['target'].isLit(), 'Target should not be lit');
+			assert.notOk(puzzle.targets['target'].isStruckBy(TestHelper.laserColors.red), 'Target should not be struck by red laser');
+			assert.notOk(puzzle.exits['exit'].isOpen, 'Exit should not be open');
+		});
+	})
+
 });
